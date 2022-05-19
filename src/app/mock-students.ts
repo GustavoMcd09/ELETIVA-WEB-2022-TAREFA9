74d1285dfe45ae5367f40db9c8e7a50add403e43
GustavoMcd09 <GustavoMcd09>
2022-05-18 22:10:09 -0300

    TAREFA 9

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -1,7 +1,7 @@
 import { Student } from './student';
 
 export const STUDENTS: Student[] = [
-  { id: 1, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jun 24 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
+  { id: 1, name: 'Túlio da Silva Silveira Arruda', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jun 24 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
   { id: 2, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('aug 12 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
   { id: 3, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('feb 3 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
   { id: 4, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jan 1 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
