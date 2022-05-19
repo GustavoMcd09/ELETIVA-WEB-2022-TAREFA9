53e84e82e6927cf4ee222ec74aa2b6b87b4c36f4
GustavoMcd09 <GustavoMcd09>
2022-05-19 20:59:15 -0300

    TAREFA 9

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -1,12 +1,12 @@
 import { Student } from './student';
 
 export const STUDENTS: Student[] = [
-  { id: 1, name: 'Túlio da Silva Silveira Arruda', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jun 24 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 2, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('aug 12 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 3, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('feb 3 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 4, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jan 1 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 5, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jul 29 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 6, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('sep 10 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 7, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('dec 15 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
-  { id: 8, name: 'Túlio da Silva', cod: 202, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('may 20 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
+  { id: 1, name: 'Túlio da Silva', cod: 201, cpf: 43989313322, rg: 455534212, mae: 'Marta da Silva', pai: 'José da Silva', nascimento: new Date('jun 24 2003 21:52:12'), sexo: 'Masculino', idade: 19 },
+  { id: 2, name: 'Emanuel Rocha Costa', cod: 202, cpf: 23221224513, rg: 123456321, mae: 'Mercia Rocha Costa', pai: 'Joel Costa', nascimento: new Date('aug 12 2002 22:32:14'), sexo: 'Masculino', idade: 20 },
+  { id: 3, name: 'Henrique Sousa', cod: 203, cpf: 26523454643, rg: 765458912, mae: 'Jennifer Sousa', pai: 'Dimas Sousa', nascimento: new Date('feb 3 2000 15:47:32'), sexo: 'Masculino', idade: 22 },
+  { id: 4, name: 'Giulia Ribeiro Perrone', cod: 204, cpf: 13245678923, rg: 987423111, mae: 'Mariana Ribeiro Perrone', pai: 'Ricardo Perrone', nascimento: new Date('jan 1 1997 09:30:09'), sexo: 'Feminino', idade: 25 },
+  { id: 5, name: 'Jeferson Mattos', cod: 205, cpf: 1234567891, rg: 122928901, mae: 'Milena Mattos', pai: 'Carlos Mattos', nascimento: new Date('jul 29 2004 10:15:13'), sexo: 'Masculino', idade: 18 },
+  { id: 6, name: 'Helena Piva', cod: 206, cpf: 9876543219, rg: 767567321, mae: 'Maria Piva', pai: 'Copérnico Piva', nascimento: new Date('sep 10 1996 20:20:20'), sexo: 'Feminino', idade: 26 },
+  { id: 7, name: 'Jéssica Moreira', cod: 207, cpf: 78654321390, rg: 676123900, mae: 'Tabata Moreira', pai: 'Paulo Moreira', nascimento: new Date('dec 15 1998 23:59:15'), sexo: 'Feminino', idade: 24 },
+  { id: 8, name: 'Miriam Lima Oliveira', cod: 208, cpf: 53453421238, rg: 115678987, mae: 'Carla Lima Oliveira', pai: 'Charles Oliveira', nascimento: new Date('may 20 1999 16:42:12'), sexo: 'Feminino', idade: 23 },
 ];
